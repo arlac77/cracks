9c93401de4f384c36dcd85a2bf97659b86b3a2a5
Markus Felten <Markus.Felten@gmx.de>
2020-01-10 12:27:41 +0100

    chore: update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,26 @@
-import pkg from './package.json';
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import executable from "rollup-plugin-executable";
+import consts from "rollup-plugin-consts";
+
 import builtins from "builtin-modules";
+import { name, version, description, main, bin } from "./package.json";
 
 export default {
   output: {
-    file: pkg.bin,
+    file: bin,
     format: 'cjs',
     banner: '#!/usr/bin/env node',
     interop: false
   },
-  plugins: [resolve(), commonjs(), json(), executable()],
+  plugins: [ consts({
+    name,
+    version,
+    description
+  }),
+ resolve(), commonjs(), executable()],
   external: [
     ...builtins
   ],
-  input: pkg.main
+  input: main
 };
